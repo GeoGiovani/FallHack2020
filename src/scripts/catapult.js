1e539a136792f61a04a41e4b4f4e292e97dc2e93
brianfu <fubof@sfu.ca>
2020-11-07 13:56:07 -0800

    cleaned

--- src/scripts/catapult.js
+++ src/scripts/catapult.js
@@ -1,101 +1,98 @@
 import Matter from "matter-js";
-const height = 600;
-const width = 800;
+const canvasHeight = 600;
+const canvasWidth = 800;
 
-
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Composites = Matter.Composites,
-    Constraint = Matter.Constraint,
-    MouseConstraint = Matter.MouseConstraint,
-    Mouse = Matter.Mouse,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-    Body = Matter.Body,
-    Vector = Matter.Vector;
+//Init
+var Engine = Matter.Engine;
+var Render = Matter.Render;
+var Runner = Matter.Runner;
+var Events = Matter.Events;
+var Composites = Matter.Composites;
+var Constraint = Matter.Constraint;
+var MouseConstraint = Matter.MouseConstraint;
+var Mouse = Matter.Mouse;
+var World = Matter.World;
+var Bodies = Matter.Bodies;
+var Body = Matter.Body;
+var Vector = Matter.Vector;
 
 // create engine
-var engine = Engine.create(),
-    world = engine.world;
+var physicsEngine = Engine.create();
+var world = physicsEngine.world; //World object can contain many worlds, we only need our 1
 
 // create renderer
-var render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        width: 800,
-        height: 600,
-        showAngleIndicator: true,
-        showCollisions: true,
-        showVelocity: true
-    }
-});
-
+//JSON Object: {Key:Value, Key:Value, ...}
+var render = Render.create({ //Create object 'render' of type Render containing rendering attributes
+                element: document.body,
+                engine: physicsEngine,
+                options: {
+                    width: 800,
+                    height: 600,
+                    showAngleIndicator: true,
+                    showCollisions: true,
+                    showVelocity: true
+                }
+            });
 Render.run(render);
 
 // create runner
 var runner = Runner.create();
-Runner.run(runner, engine);
+Runner.run(runner, physicsEngine); //void
 
 // add bodies
 var group = Body.nextGroup(true);
 
-var stack = Composites.stack(250, 255, 1, 6, 0, 0, function (x, y) {
-    return Bodies.rectangle(x, y, 30, 30);
-});
-
+//World Objects
+var rectangles = Composites.stack(250, 255, 1, 6, 0, 0, //Bunch of rectangles
+    function (x, y){return Bodies.rectangle(x, y, 30, 30);}
+    ); //last arg is effective lambda
 var catapult = Bodies.rectangle(400, 520, 320, 20, { collisionFilter: { group: group } });
-var ground = Bodies.rectangle(395, 600, 815, 50, { isStatic: true }),
-rockOptions = { density: 0.004 },
-rock = Bodies.polygon(170, 450, 8, 20, rockOptions),
-anchor = { x: 170, y: 450 },
-elastic = Constraint.create({ 
+var ground = Bodies.rectangle(395, 600, 815, 50, { isStatic: true });
+var rockOptions = {density: 0.004};
+var rock = Bodies.polygon(170, 450, 8, 20, rockOptions);
+var anchor = {x: 170, y: 450};
+var elastic = Constraint.create({
     pointA: anchor, 
     bodyB: rock, 
     stiffness: 0.05
 });
+var rect1 = Bodies.rectangle(400, 600, 800, 50.5, {isStatic: true});
+var rect2 = Bodies.rectangle(250, 555, 20, 50, {isStatic: true });
+var rect3 = Bodies.rectangle(400, 535, 20, 80, {isStatic: true, collisionFilter: {group: group}});
+var circle1 = Bodies.circle(560, 100, 50, {density: 0.005});
 
-World.add(world, [
-    stack,
-    catapult,
-    Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true }),
-    Bodies.rectangle(250, 555, 20, 50, { isStatic: true }),
-    Bodies.rectangle(400, 535, 20, 80, { isStatic: true, collisionFilter: { group: group } }),
-    Bodies.circle(560, 100, 50, { density: 0.005 }),
+//Walls
+//Bodies.rectangle(x, y, width, height, {optionsJSON})
+var bottomWall = Bodies.rectangle(canvasWidth/2, 0, canvasWidth, 1, {label: 'wall', isStatic: true});
+var topWall = Bodies.rectangle(canvasWidth/2, canvasHeight, canvasWidth, 1, {label: 'wall',isStatic: true});
+var rightWall = Bodies.rectangle(canvasWidth, canvasHeight/2, 1, canvasHeight, {label: 'wall',isStatic: true});
+var leftWall = Bodies.rectangle(0, canvasHeight/2, 1, canvasHeight, {label: 'wall',isStatic: true});
+var worldConstraint = Constraint.create({
+                                    bodyA: catapult,
+                                    pointB: Vector.clone(catapult.position),
+                                    stiffness: 1,
+                                    length: 0
+                                });
 
-    //This is for the walls
-    Bodies.rectangle(width/2, 0,               width, 1,              { label: 'wall',isStatic: true }),   //bottom
-    Bodies.rectangle(width/2, height,   width, 1,              { label: 'wall',isStatic: true }),   //top
-    Bodies.rectangle(width,   height/2, 1,            height,  { label: 'wall',isStatic: true }),   //right
-    Bodies.rectangle(0,              height/2, 1,            height,  { label: 'wall',isStatic: true }),    //left
-    ground , rock, elastic,
+//Mouse
 
 
-    Constraint.create({
-        bodyA: catapult,
-        pointB: Vector.clone(catapult.position),
-        stiffness: 1,
-        length: 0
-    })
-]);
 
-// add mouse control
-var mouse = Mouse.create(render.canvas),
-    mouseConstraint = MouseConstraint.create(engine, {
-        mouse: mouse,
+
+World.add(world, [rectangles, catapult, ground, rockOptions, rock, anchor, elastic, rect1, rect2, rect3, circle1, bottomWall, topWall, rightWall, leftWall, worldConstraint]);
+
+//Mouse --must be rendered after
+var mouse = Mouse.create(render.canvas); // add mouse control
+var mouseConstraint = MouseConstraint.create(physicsEngine, //must be added to world
+    {mouse: mouse,
         constraint: {
             stiffness: 0.2,
-            render: {
-                visible: false
-            }
+            render: {visible: false}
         }
     });
 
-World.add(world, mouseConstraint);
-
-// keep the mouse in sync with rendering
-render.mouse = mouse;
+World.add(world, mouseConstraint)
+render.mouse = mouse; //keep the mouse in sync with rendering
 
 // fit the render viewport to the scene
 Render.lookAt(render, {
