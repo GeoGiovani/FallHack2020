5d68a3a5fa92ba9870b229469118453e25a6a4f4
Tanish Manku <tanish2k09.dev@gmail.com>
2020-11-07 12:45:22 -0800

    Catapult: Make it working
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

--- src/scripts/catapult.js
+++ src/scripts/catapult.js
@@ -1,15 +1,83 @@
-import { Engine, World, Bodies } from "matter-js";
+import Matter from "matter-js";
 
-// create a Matter.js engine
-var engine = Engine.create(document.getElementById('root'));
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composites = Matter.Composites,
+    Constraint = Matter.Constraint,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Vector = Matter.Vector;
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+// create engine
+var engine = Engine.create(),
+    world = engine.world;
 
-// add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+// create renderer
+var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 800,
+        height: 600,
+        showAngleIndicator: true,
+        showCollisions: true,
+        showVelocity: true
+    }
+});
 
-// run the engine
-Engine.run(engine);
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+Runner.run(runner, engine);
+
+// add bodies
+var group = Body.nextGroup(true);
+
+var stack = Composites.stack(250, 255, 1, 6, 0, 0, function (x, y) {
+    return Bodies.rectangle(x, y, 30, 30);
+});
+
+var catapult = Bodies.rectangle(400, 520, 320, 20, { collisionFilter: { group: group } });
+
+World.add(world, [
+    stack,
+    catapult,
+    Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true }),
+    Bodies.rectangle(250, 555, 20, 50, { isStatic: true }),
+    Bodies.rectangle(400, 535, 20, 80, { isStatic: true, collisionFilter: { group: group } }),
+    Bodies.circle(560, 100, 50, { density: 0.005 }),
+    Constraint.create({
+        bodyA: catapult,
+        pointB: Vector.clone(catapult.position),
+        stiffness: 1,
+        length: 0
+    })
+]);
+
+// add mouse control
+var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+                visible: false
+            }
+        }
+    });
+
+World.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+// fit the render viewport to the scene
+Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: 800, y: 600 }
+});
