40c72e36d4e0e27711d03bcdbba6c80cd6d152c7
Tanish Manku <tanish2k09.dev@gmail.com>
2020-11-07 12:24:39 -0800

    Catapult: Fix compilation
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+require('./scripts/catapult');
