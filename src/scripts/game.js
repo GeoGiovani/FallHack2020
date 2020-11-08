8e960af8eee900809d2b8be9e9252f13ca7a26a3
Tanish Manku <tanish2k09.dev@gmail.com>
2020-11-07 17:07:18 -0800

    Define a background
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,4 +1,6 @@
-import Matter from "matter-js";
+import Matter, { Composite } from "matter-js";
+import BACKGROUND from "./background.png";
+
 const canvasHeight = 600;
 const canvasWidth = 800;
 
@@ -22,7 +24,7 @@ var world = physicsEngine.world; //World object can contain many worlds, we only
 
 // create renderer
 //JSON Object: {Key:Value, Key:Value, ...}
-var render = Render.create({ //Create object 'render' of type Render containing rendering attributes
+var render = Render.create({ //Create object  'render' of type Render containing rendering attributes
     element: document.body,
     engine: physicsEngine,
     options: {
@@ -30,23 +32,28 @@ var render = Render.create({ //Create object 'render' of type Render containing 
         height: 600,
         showAngleIndicator: true,
         showCollisions: true,
-        showVelocity: true
+        showVelocity: true,
+        background: BACKGROUND,
+        wireframes: false
     }
 });
 Render.run(render);
+// create runner
+var runner = Runner.create();
+Runner.run(runner, physicsEngine); //void
 
 //World Objects
 var collisionGroup1 = Body.nextGroup(true); //groups for collision
-var ground = Bodies.rectangle(395, 600, 815, 50, {isStatic: true});
-var rect1 = Bodies.rectangle(150, 270, 40, 50, {isStatic: true});
+var ground = Bodies.rectangle(395, 600, 815, 50, { isStatic: true });
+var rect1 = Bodies.rectangle(150, 270, 40, 50, { isStatic: true });
 
 
 //Walls
 //Bodies.rectangle(x, y, width, height, {optionsJSON})
-var bottomWall = Bodies.rectangle(canvasWidth/2, 0, canvasWidth, 1, {label: 'wall', isStatic: true});
-var topWall = Bodies.rectangle(canvasWidth/2, canvasHeight, canvasWidth, 1, {label: 'wall',isStatic: true});
-var rightWall = Bodies.rectangle(canvasWidth, canvasHeight/2, 1, canvasHeight, {label: 'wall',isStatic: true});
-var leftWall = Bodies.rectangle(0, canvasHeight/2, 1, canvasHeight, {label: 'wall',isStatic: true});
+var bottomWall = Bodies.rectangle(canvasWidth / 2, 0, canvasWidth, 1, { label: 'wall', isStatic: true });
+var topWall = Bodies.rectangle(canvasWidth / 2, canvasHeight, canvasWidth, 1, { label: 'wall', isStatic: true });
+var rightWall = Bodies.rectangle(canvasWidth, canvasHeight / 2, 1, canvasHeight, { label: 'wall', isStatic: true });
+var leftWall = Bodies.rectangle(0, canvasHeight / 2, 1, canvasHeight, { label: 'wall', isStatic: true });
 
 
 //Main world add
@@ -55,10 +62,11 @@ World.add(world, [ground, rect1, bottomWall, topWall, rightWall, leftWall]);
 //Mouse --must be rendered after
 var mouse = Mouse.create(render.canvas); // add mouse control
 var mouseConstraint = MouseConstraint.create(physicsEngine, //must be added to world
-    {mouse: mouse,
+    {
+        mouse: mouse,
         constraint: {
             stiffness: 0.2,
-            render: {visible: false}
+            render: { visible: false }
         }
     });
 
@@ -70,3 +78,149 @@ Render.lookAt(render, {
     min: { x: 0, y: 0 },
     max: { x: 800, y: 600 }
 });
+
+Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: 800, y: 600 }
+});
+
+export function moveLeft(playerNum) {
+    if (movementRight[playerNum] !== null) {
+        clearTimeout(movementRight[playerNum])
+        movementRight[playerNum] = null
+    }
+
+    if (movementLeft[playerNum] !== null) {
+        return
+    }
+
+    moveLeftInternal(playerNum)
+}
+
+function moveLeftInternal(playerNum) {
+    Body.setVelocity(players[playerNum], {
+        x: -3,
+        y: players[playerNum].velocity.y
+    })
+
+    movementLeft[playerNum] = setTimeout(moveLeftInternal, 16, playerNum);
+}
+
+export function moveRight(playerNum) {
+    if (movementLeft[playerNum] !== null) {
+        clearTimeout(movementLeft[playerNum])
+        movementLeft[playerNum] = null
+    }
+
+    if (movementRight[playerNum] !== null) {
+        movementRight[playerNum] = null
+        return
+    }
+
+    moveRightInternal(playerNum)
+}
+
+function moveRightInternal(playerNum) {
+    Body.setVelocity(players[playerNum], {
+        x: 3,
+        y: players[playerNum].velocity.y
+    })
+
+    movementRight[playerNum] = setTimeout(moveRight, 16, playerNum);
+}
+
+export function moveUp(playerNum) {
+    if (state[playerNum] !== "jumping") {
+        Body.setVelocity(players[playerNum], {
+            x: players[playerNum].velocity.x,
+            y: -8
+        })
+
+        state[playerNum] = "jumping"
+        setTimeout(function () {
+            state[playerNum] = "grounded"
+        }, 2000)
+    }
+}
+
+export function stopLeft(playerNum) {
+    if (movementLeft[playerNum] !== null) {
+        clearTimeout(movementLeft[playerNum])
+        movementLeft[playerNum] = null
+    }
+}
+
+export function stopRight(playerNum) {
+    if (movementRight[playerNum] !== null) {
+        clearTimeout(movementRight[playerNum])
+        movementRight[playerNum] = null
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
